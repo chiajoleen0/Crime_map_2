39da268455713a52f4f20293c413712d77ddf9d6
chiajoleen0 <chiajoleen0@gmail.com>
2025-08-02 22:22:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const map = L.map('map').setView([1.3521, 103.8198], 12);
 
+// Replace <YOUR_TOKEN> with your OneMap API token
+const tileUrl = `https://tiles.onemap.sg/v3/Default/{z}/{x}/{y}.png?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo4MTI0LCJmb3JldmVyIjpmYWxzZSwiaXNzIjoiT25lTWFwIiwiaWF0IjoxNzU0MTQ0Mzc2LCJuYmYiOjE3NTQxNDQzNzYsImV4cCI6MTc1NDQwMzU3NiwianRpIjoiYTljNzc5YzktMjk3NS00Zjg2LThiMTgtZjhmZGZiZjc2MjY1In0.JJeGkgNtX1XnFusye2Z1yetH9--8v47_H_IFTmXD60-NhCoHCuHJ1kZChqyNpjGPhREns1wgDphfzBIxlZNXfaASxuxSKGFRSRoIaawMw4iVqPLnaWrusc6AoX4QyUUt8ZvXBW0IEFRPutqwE1hi_AHFA-aGC6ik_1cLYLUrBRYABxXV1FTA99F0azYdp-wakXvwdmq2nW4MBHM6Mr7MKgJA0k8QOCDZrzgw8PAEmH-EsJLctO6LiYbmcG9GgyNUCcrEC4Q8m9SSZGT8GAKrw26OBeDC6G5Ta1ST3FiNVV4r-H5UIQacVNAAEWiIHPl3KLv5QtgCfkhfAbMvdZKHYw`;
+
+L.tileLayer(tileUrl, {
+  maxZoom: 18,
+  attribution: 'Map data © SLA OneMap'
+}).addTo(map);
+
+L.marker([1.353,103.82]).addTo(map)
+  .bindPopup("Example Hotspot")
+  .openPopup();
+
 // OneMap base layer
 L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
   attribution: 'Map data © Singapore Land Authority',
